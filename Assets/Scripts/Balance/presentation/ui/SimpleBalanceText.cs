f086402dfe50eaed1c6447081958d6723fe639a3
Denis Rebrov <denisrebrof@gmail.com>
2021-12-10 11:16:15 +0300

    коммент

---
+++ Assets/Scripts/Balance/presentation/ui/SimpleBalanceText.cs
@@ -0,0 +1,22 @@
+﻿using Balance.domain;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Balance.presentation.ui
+{
+    [RequireComponent(typeof(Text))]
+    public class SimpleBalanceText : MonoBehaviour
+    {
+        [Inject] private IBalanceRepository balanceRepository;
+        private Text target;
+
+        private void Awake() => target = GetComponent<Text>();
+
+        private void OnEnable()
+        {
+            var balance = balanceRepository.GetBalance();
+            target.text = balance.ToString();
+        }
+    }
+}
