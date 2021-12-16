ee1111d28d923608eb3e3704c41ac9b24acdd81f
Denis Rebrov <denisrebrof@gmail.com>
2021-12-16 19:05:38 +0300

    added previous level use case

---
+++ Assets/Scripts/Levels/domain/GetPreviousLevelUseCase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Levels.data.dao;
+using Levels.domain.model;
+using Levels.domain.repositories;
+using Zenject;
+
+namespace Levels.domain
+{
+    public class GetPreviousLevelUseCase
+    {
+        [Inject] private ICurrentLevelRepository currentLevelRepository;
+        [Inject] private ILevelsRepository levelsRepository;
+
+        private int GetCurrentLevelIndex()
+        {
+            var currentLevelId = currentLevelRepository.GetCurrentLevel().ID;
+            return levelsRepository.GetLevels().FindIndex(level => level.ID == currentLevelId);
+        }
+
+        public bool GetHasPreviousLevel() => GetCurrentLevelIndex() > 0;
+        
+        public Level GetPreviousLevel()
+        {
+            var currentIndex = GetCurrentLevelIndex();
+            return levelsRepository.GetLevels()[currentIndex-1];
+        }
+    }
+}
